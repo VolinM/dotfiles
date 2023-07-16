4697f2d16a171fb70956f916135707fbc8446361
VolinM <markvolin.br@gmail.com>
2023-07-16 17:16:19 -0400

    configured matrix snippet

--- nvim/snippets/tex/math.lua
+++ nvim/snippets/tex/math.lua
@@ -34,6 +34,27 @@ end
 
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
+-- MATRIX generator
+local mat = function(args, snip)
+  local rows = tonumber(snip.captures[2])
+  local cols = tonumber(snip.captures[3])
+  local nodes = {}
+  local ins_indx = 1
+  for j = 1, rows do
+    table.insert(nodes, r(ins_indx, tostring(j) .. "x1", i(1)))
+    ins_indx = ins_indx + 1
+    for k = 2, cols do
+      table.insert(nodes, t(" & "))
+      table.insert(nodes, r(ins_indx, tostring(j) .. "x" .. tostring(k), i(1)))
+      ins_indx = ins_indx + 1
+    end
+    table.insert(nodes, t({ " \\\\", "" }))
+  end
+  -- fix last node.
+  nodes[#nodes] = t(" \\\\")
+  return sn(nil, nodes)
+end
+
 local M = {
   -------------------------------
   -- BASIC OPERATORS
@@ -508,6 +529,40 @@ local M = {
   -- MATRICES
   -----------------------------
 
+  -- GENERATING MATRICES
+  s(
+    {
+      trig = "([bBpvV])mat(%d+)x(%d+)([ar])",
+      regTrig = true,
+      name = "matrix",
+      dscr = "matrix trigger lets go",
+      snippetType = "autosnippet",
+    },
+    fmt(
+      [[
+    \begin{<>}<>
+    <>
+    \end{<>}]],
+      {
+        f(function(_, snip)
+          return snip.captures[1] .. "matrix" -- captures matrix type
+        end),
+        f(function(_, snip)
+          if snip.captures[4] == "a" then
+            local out = string.rep("c", tonumber(snip.captures[3]) - 1) -- array for augment
+            return "[" .. out .. "|c]"
+          end
+          return "" -- otherwise return nothing
+        end),
+        d(1, mat),
+        f(function(_, snip)
+          return snip.captures[1] .. "matrix" -- i think i could probably use a repeat node but whatever
+        end),
+      },
+      { delimiters = "<>" }
+    ),
+    { condition = tex.in_mathzone }
+  ),
   -- CDOTS, i.e. \cdots
   s({ trig = "cdd", snippetType = "autosnippet" }, {
     t("\\cdots"),

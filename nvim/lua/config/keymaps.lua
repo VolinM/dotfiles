c049a64b5586d17ab3e4f72932a2ff7b2c4e2dcf
VolinM <markvolin.br@gmail.com>
2023-07-18 10:31:07 -0400

    keymaps to jump in insert mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
 vim.keymap.set(
   "n",
   "<leader>sx",
@@ -19,8 +22,11 @@ vim.keymap.set(
   "<C-f>",
   [[<Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR> ]]
 )
-vim.keymap.set(
-  "n",
-  "<C-f>",
-  [[: silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>]]
-)
+-- vim.keymap.set(
+--   "n",
+--   "<C-f>",
+--   [[: silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>]]
+--)
+--
+map("i", "<S-Right>", "<Esc>$a")
+map("i", "<S-Left>", "<Esc>^i")

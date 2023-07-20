afbd28a5120dddc75c2d009e21a5d1428d74ebea
VolinM <markvolin.br@gmail.com>
2023-07-20 17:37:28 -0400

    switched to nord bar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,22 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        # get the screen rotation
-        rot=$(xrandr --verbose | grep $m | egrep -o " (normal) ")
-        # select the bar name wrt rotation
-        if [[ $rot == " normal " ]]; then
-            bar_name="mybar"
-        else
-            bar_name="mybar_vert"
-        fi
-        # launch the bar on the correct monitor
-        MONITOR=$m polybar --reload $bar_name &
-    done
+# Launch bar1 and bar2
+if [ "$1" == "light" ]; then
+	polybar -c $HOME/.config/polybar/bars/light-config nord-top &
+	polybar -c $HOME/.config/polybar/bars/light-config nord-down &
 else
-    polybar --reload mybar &
+	polybar -c $HOME/.config/polybar/bars/dark-config nord-top &
+	# polybar -c $HOME/.config/polybar/bars/dark-config nord-down &
 fi
 
-echo "Polybar launched..."
+echo "Bars launched..."
